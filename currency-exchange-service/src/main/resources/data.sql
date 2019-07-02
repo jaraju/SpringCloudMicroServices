f90100d90e54fab5f08e651f5d8942e1b182df16
Appalaraju Jampana <raju@Appalarajus-MacBook-Pro.local>
2019-07-02 00:16:14 -0400

    First Commit

---
+++ currency-exchange-service/src/main/resources/data.sql
@@ -0,0 +1,4 @@
+insert into exchange_value(id,curreny_from,curreny_to,conversion_multiple,port) values(10001,'USD','INR',65,0);
+insert into exchange_value(id,curreny_from,curreny_to,conversion_multiple,port) values(10002,'EUR','INR',55,0);
+insert into exchange_value(id,curreny_from,curreny_to,conversion_multiple,port) values(10003,'CAD','INR',45,0);
+insert into exchange_value(id,curreny_from,curreny_to,conversion_multiple,port) values(10004,'AUD','INR',35,0);
